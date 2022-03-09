8a7f0a292b083b6c2962e307ee543e56a8d5aced
Szymon <wojdyla.szymon@gmail.com>
2022-03-09 15:25:36 +0000

    extensions

--- controllers/controller.py
+++ controllers/controller.py
@@ -6,3 +6,8 @@ from models.product_list import orders
 def index():
     return render_template("index.html", title="Home", orders=orders)
     
+@app.route('/orders/<id>')
+def order(id):
+    coffee = orders[int(id)]
+    return render_template("order.html", id=id, title="Ordered items", order=coffee)
+    # return "Hello World"
